7307923ee981c05a18bc0bb4b50669d40f671315
jdhopia <43420804+jdhopia@users.noreply.github.com>
2024-06-16 20:03:24 -0500

    Update interpreter.py

--- interpreter.py
+++ interpreter.py
@@ -4,7 +4,5 @@ class Interpreter:
         self.account_dict = account_dict
 
     def interpret(self):
-        # implement your interpretation logic.
-        # For now, print out AST and account dictionary.
         print("AST:", self.ast)
         print("Accounts:", self.account_dict)
