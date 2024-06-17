2dd33e2164005580d3b9ff07db91a51f3b41e8df
lucasvbecker <lucasvbecker99@gmail.com>
2024-06-16 19:41:14 -0500

    Update parser.py

--- parser.py
+++ parser.py
@@ -1,21 +1,26 @@
 class Parser:
-    def __init__(self, tokens):
-        # Initialize the parser with the tokens from the lexer
-        self.tokens = tokens
-        self.current_token = None
+    def __init__(self, lexer):
+        self.lexer = lexer
+        self.current_token = self.lexer.get_next_token()
+
+    def error(self):
+        raise Exception('Invalid syntax')
 
     def eat(self, token_type):
-        # Check if the current token's type matches the expected token_type
-        # If it matches, "eat" the current token and assign the next token to self.current_token
+        if self.current_token.type == token_type:
+            self.current_token = self.lexer.get_next_token()
+        else:
+            self.error()
+
+    def statement(self):
+        # Implement according to your language's grammar
         pass
+
+    def program(self):
+        nodes = self.statement_list()
+        if self.current_token.type != 'EOF':
+            self.error()
+        return nodes
 
     def parse(self):
-        # The main method of the Parser class that organizes the tokens into an AST
-        pass
-
-    # Add methods for each grammar rule here
-    # For example, if your grammar has a rule for expressions like:
-    # expression : term ((PLUS | MINUS) term)*
-    # You would add a method like this:
-    def expression(self):
-        pass
+        return self.program()
