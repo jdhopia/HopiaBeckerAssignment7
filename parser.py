05a9b568adae1766909eaaf0aeb3d8106c82c6c4
lucasvbecker <lucasvbecker99@gmail.com>
2024-06-16 19:51:13 -0500

    Update parser.py

--- parser.py
+++ parser.py
@@ -1,26 +1,16 @@
 class Parser:
-    def __init__(self, lexer):
-        self.lexer = lexer
-        self.current_token = self.lexer.get_next_token()
+    def __init__(self, tokens):
+        self.tokens = tokens
+        self.current_token = None
+        self.next_token()
 
-    def error(self):
-        raise Exception('Invalid syntax')
-
-    def eat(self, token_type):
-        if self.current_token.type == token_type:
-            self.current_token = self.lexer.get_next_token()
-        else:
-            self.error()
-
-    def statement(self):
-        # Implement according to your language's grammar
-        pass
-
-    def program(self):
-        nodes = self.statement_list()
-        if self.current_token.type != 'EOF':
-            self.error()
-        return nodes
+    def next_token(self):
+        try:
+            self.current_token = next(self.tokens)
+        except StopIteration:
+            self.current_token = None
 
     def parse(self):
-        return self.program()
+        # This is where you would implement your parsing logic.
+        # For now, we'll just return the tokens as a list.
+        return [token for token in self.tokens]
