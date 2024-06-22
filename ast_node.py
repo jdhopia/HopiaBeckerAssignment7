cf5fe2fed15175ebde4c9edcbb86bd65355a4a2b
jdhopia <43420804+jdhopia@users.noreply.github.com>
2024-06-22 15:33:04 -0500

    Update ast_node.py

--- ast_node.py
+++ ast_node.py
@@ -1,1 +1,9 @@
+from token import Token
 
+class ASTNode:
+    def __init__(self, token: Token):
+        self.token = token
+        self.children = []
+
+    def add_child(self, node):
+        self.children.append(node)
