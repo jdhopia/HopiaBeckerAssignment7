9ff4e11ef85489ffaa170cd0aa8e8515720f0593
jdhopia <43420804+jdhopia@users.noreply.github.com>
2024-06-22 15:33:56 -0500

    Update token.py

--- token.py
+++ token.py
@@ -1,1 +1,7 @@
+class Token:
+    def __init__(self, type_: str, value: str):
+        self.type = type_
+        self.value = value
 
+    def __repr__(self):
+        return f"Token({self.type}, {self.value})"
