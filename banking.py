ea6f53a263298ce47802823355dec93482598aee
jdhopia <43420804+jdhopia@users.noreply.github.com>
2024-06-23 15:37:00 -0500

    Update banking.py

--- banking.py
+++ banking.py
@@ -1,24 +1,29 @@
-from bank_account import BankAccount
-from lexer import Lexer
-from parser import Parser
+"""
+Author: Lucas Becker and Jayce Hopia
+Date: 06/23/2024
+Class: 330
+Description: This file contains the main program for the Banking DSL.
+Pledge: This is all our own work.
+"""
+
+from my_lexer import Lexer
+from my_parser import Parser
 from interpreter import Interpreter
 
 def main():
-    accounts = [
-        BankAccount("Alice", "Smith", 1000),
-        BankAccount("Bob", "Brown", 1500),
-        BankAccount("Charlie", "Davis", 2000)
-    ]
-
-    lexer = Lexer("create deposit withdraw balance")
+    input_text = """
+    CREATE ACCOUNT Dawn,Duerre,DD330330,100000000.00
+    DEPOSIT 500000 TO DD330330
+    WITHDRAW 200 FROM DD330330
+    BALANCE OF DD330330
+    EXIT
+    """
+    lexer = Lexer(input_text)
     tokens = lexer.tokenize()
     parser = Parser(tokens)
     ast = parser.parse()
     interpreter = Interpreter(ast)
     interpreter.interpret()
-
-    for account in accounts:
-        print(account)
 
 if __name__ == "__main__":
     main()
