84cb73255c70a534a27cf986b0b35d83efa75299
jdhopia <43420804+jdhopia@users.noreply.github.com>
2024-06-23 15:35:40 -0500

    Update ast_node.py

--- ast_node.py
+++ ast_node.py
@@ -1,9 +1,19 @@
-from token import Token
+"""
+Author: Jayce Hopia
+Date: 06/23/2024
+Class: 330
+Description: This file contains the ASTNode class for the Banking DSL.
+Pledge: This work is all my own with the help of Lucas.
+"""
 
 class ASTNode:
-    def __init__(self, token: Token):
-        self.token = token
+    def __init__(self, node_type, value=None):
+        self.node_type = node_type
+        self.value = value
         self.children = []
 
     def add_child(self, node):
         self.children.append(node)
+
+    def __repr__(self):
+        return f"ASTNode({self.node_type}, {self.value}, {self.children})"
